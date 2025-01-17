4323b0dc6649862c67d482394a9d0969c0f14b80
Chris Armstrong <chris-armstrong@users.noreply.github.com>
2025-01-17 23:02:48 +1100

    recompose incorporating playground changes

--- packages/ocaml-cross/ocaml-cross.5.2.0/files/install.sh
+++ packages/ocaml-cross/ocaml-cross.5.2.0/files/install.sh
@@ -3,6 +3,7 @@
 set -ex
 
 PREFIX=$1
+HOST_SWITCH=$2
 if [ ! -d "${PREFIX}" ]
 then
 	echo "Prefix directory \"$PREFIX\" does not exist"
@@ -17,40 +18,7 @@ mkdir -p $PREFIX/lib/ocaml/stublibs
 mkdir -p $PREFIX/lib/stublibs
 
 echo "-- copying compiler tooling to $PREFIX"
-cp ocaml $PREFIX/bin
-cp ocamlc $PREFIX/bin
-cp ocamlopt $PREFIX/bin
-cp boot/ocamlrun $PREFIX/bin
-cp tools/ocamlmklib $PREFIX/bin
-cp tools/ocamldep $PREFIX/bin
-cp tools/ocamlobjinfo $PREFIX/bin
-cp tools/ocamlcmt $PREFIX/bin
-cp tools/ocamlcp $PREFIX/bin
-cp tools/ocamlmktop $PREFIX/bin
-cp tools/ocamloptp $PREFIX/bin
-cp Makefile.config $PREFIX/lib/ocaml
-
-echo "-- copy libcamlrun"
-# cp boot/libcamlrun.a $PREFIX/lib/ocaml
-# cp boot/libcamlrun_shared.so $PREFIX/lib/ocaml
-cp runtime/libcamlrun* $PREFIX/lib/ocaml
-echo "-- copy libasmrun*"
-cp runtime/libasmrun* $PREFIX/lib/ocaml
-echo "-- copy libcomprmarsh*"
-cp runtime/libcomprmarsh* $PREFIX/lib/ocaml
-
-echo "-- install stdlib"
-make -C stdlib install installopt
-echo "-- install dynlink"
-make -C otherlibs/dynlink install installopt
-echo "-- install runtime_events"
-make -C otherlibs/runtime_events install installopt
-echo "-- install str"
-make -C otherlibs/str install installopt
-echo "-- install systhreads"
-make -C otherlibs/systhreads install installopt
-echo "-- install unix"
-make -C otherlibs/unix install installopt
+OCAMLRUN="$HOST_SWITCH/bin/ocamlrun" make install
 
 cat << EOF > $PREFIX/lib/findlib.conf
 destdir="$PREFIX/lib"
