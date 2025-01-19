9eed435bf4109809c6d93bbbc76ab35c523169a5
Chris Armstrong <chris-armstrong@users.noreply.github.com>
2025-01-19 11:47:05 +1100

    filter out existing findlib.conf

--- packages/ocaml-cross/ocaml-cross.5.2.0/files/install.sh
+++ packages/ocaml-cross/ocaml-cross.5.2.0/files/install.sh
@@ -4,9 +4,22 @@ set -ex
 
 PREFIX=$1
 HOST_SWITCH=$2
+CROSS_NAME=$3
 if [ ! -d "${PREFIX}" ]
 then
 	echo "Prefix directory \"$PREFIX\" does not exist"
+	exit 1
+fi
+
+if [ ! -d "${HOST_SWITCH}" ]
+then
+	echo "Host switch directory \"$HOST_SWITCH\" does not exist"
+	exit 1
+fi
+
+if [ -z "$CROSS_NAME" ]
+then
+	echo "A toolchain name must be specified"
 	exit 1
 fi
 
@@ -17,18 +30,21 @@ mkdir -p $PREFIX/lib/ocaml/caml
 mkdir -p $PREFIX/lib/ocaml/stublibs
 mkdir -p $PREFIX/lib/stublibs
 
-echo "-- copying compiler tooling to $PREFIX"
+echo "-- installing compiler tooling to $PREFIX"
 OCAMLRUN="$HOST_SWITCH/bin/ocamlrun" make install
 
-cat << EOF > $PREFIX/lib/findlib.conf
-destdir="$PREFIX/lib"
-path="$PREFIX/lib:$PREFIX/lib/ocaml"
-ocamlc="ocamlc"
-ocamlopt="ocamlopt"
-ocamldep="ocamldep"
-ocamldoc="ocamldoc"
-stdlib="$PREFIX/lib/ocaml"
-ldconf="$PREFIX/ld.conf"
+echo "-- seting up ocamlfind config for host switch $HOST_SWITCH with toolchain $CROSS_NAME"
+mv $HOST_SWITCH/lib/findlib.conf $HOST_SWITCH/lib/findlib.conf.bak
+sed -e "/(crossg)/d" $HOST_SWITCH/lib/findlib.conf.bak > $HOST_SWITCH/lib/findlib.conf
+cat << EOF >> $HOST_SWITCH/lib/findlib.conf
+path($CROSS_NAME)="$PREFIX/lib:$PREFIX/lib/ocaml"
+destdir($CROSS_NAME)="$PREFIX/lib"
+stdlib($CROSS_NAME)="$PREFIX/lib/ocaml"
+ocamlc($CROSS_NAME)="ocamlc"
+ocamlopt($CROSS_NAME)="ocamlopt"
+ocamldep($CROSS_NAME)="ocamldep"
+ocamlmklib($CROSS_NAME)="ocamlmklib"
+ldconf($CROSS_NAME)="$PREFIX/lib/ocaml/ld.conf"
 EOF
 
 cat << EOF > $PREFIX/lib/ocaml/ld.conf
