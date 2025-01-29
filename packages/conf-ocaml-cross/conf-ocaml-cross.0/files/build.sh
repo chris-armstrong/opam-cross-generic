b4c45602ae603f66dc6943f80772ef354f087bba
Chris Armstrong <chris-armstrong@users.noreply.github.com>
2025-01-29 22:12:29 +0000

    move to build.sh

---
+++ packages/conf-ocaml-cross/conf-ocaml-cross.0/files/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+$ZIG_PATH version || (echo Zig binary at ZIG_PATH=$ZIG_PATH not runnable && exit 1)
+echo "" > conf-ocaml-cross.config
+echo "host_triple: \"${HOST_TRIPLE}\"" >> conf-ocaml-cross.config
+echo "host_target: \"${HOST_TARGET}\"" >> conf-ocaml-cross.config
+echo "zig_path: \"${ZIG_PATH}\"" >> conf-ocaml-cross.config
