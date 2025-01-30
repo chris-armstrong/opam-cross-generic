8ac3b0bfd02cdab184fd9b6d8fdb2d406ae3010d
Chris Armstrong <chris-armstrong@users.noreply.github.com>
2025-01-31 09:49:53 +1100

    fix line endings

--- packages/conf-ocaml-cross/conf-ocaml-cross.0/files/build.sh
+++ packages/conf-ocaml-cross/conf-ocaml-cross.0/files/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-$ZIG_PATH version || (echo "Zig binary at ZIG_PATH=$ZIG_PATH not runnable" && exit 1)
+
+"$ZIG_PATH" version || (echo "Zig binary at ZIG_PATH=$ZIG_PATH not runnable" && exit 1)
 echo "" > conf-ocaml-cross.config
 echo "host_triple: \"${HOST_TRIPLE}\"" >> conf-ocaml-cross.config
 echo "host_target: \"${HOST_TARGET}\"" >> conf-ocaml-cross.config
